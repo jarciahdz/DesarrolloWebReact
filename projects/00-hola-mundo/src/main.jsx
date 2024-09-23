c4a81f5fc47b9a8d7ad639311b3d39240c170515
chucho111 <jarcia@bancolombia.com.co>
2024-09-22 22:41:36 -0500

    primera Clase

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { App } from './App'
+const root = createRoot(document.getElementById('root'))
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+root.render(
+<App / >
+  )
